e395c2d682ee7a20a14fc713279c84f405d1a0bd
PrasannaVigneswari <prasannavigneswari@gmail.com>
2023-09-30 20:28:04 +0530

    tictactoe

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TicTacToe from './component/TicTacToe/TicTacToe';
+
+function App() {
+  return (
+    <div >
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
